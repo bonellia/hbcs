09c8cfa22e499eb5d3bc225398bd6ead5acda98c
Taha Yasin Erol <taha@bigtaus.com>
2020-10-11 13:00:51 +0300

    Add comments to highlight test arrangements.

--- Assignment.Tests/Store_CreateProductShould.cs
+++ Assignment.Tests/Store_CreateProductShould.cs
@@ -13,20 +13,26 @@ namespace Assignment.Tests
         [Fact]
         public void CreateProduct_NonExistentProductInput_ReturnSuccessMessage()
         {
+            // Arrange the case scenario:
             var testStore = new Store();
 
+            // Act using the subject method:
             string actualMessage = testStore.CreateProduct("P1", 100, 1000);
 
+            // Assert the expected results.
             Assert.Equal("Product created; code P1, price 100, stock 1000", actualMessage);            
         }
         [Fact]
         public void CreateProduct_ExistingProductInput_ReturnFailMessage()
         {
+            // Arrange the case scenario:
             var testStore = new Store();
             testStore.CreateProduct("P1", 100, 1000);
 
+            // Act using the subject method:
             string createProductResponse = testStore.CreateProduct("P1", 100, 1000);
 
+            // Assert the expected results.
             Assert.Equal("A product with the code P1 already exists.", createProductResponse);
         }
     }
