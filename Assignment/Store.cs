79daec8dbf8357db052cdcfa38ffa2ba0281b17c
Taha Yasin Erol <taha@bigtaus.com>
2020-10-11 10:57:29 +0300

    Add GetProduct & GetCampaign. Update xml comments.

--- Assignment/Store.cs
+++ Assignment/Store.cs
@@ -41,11 +41,16 @@ namespace Assignment
         /// <para>Creates a new product with the given parameters.</para>
         /// <para>Newly created product is then added to the products dictionary.</para>
         /// </summary>
-        /// <param name="productCode"></param>
-        /// <param name="price"></param>
-        /// <param name="stock"></param>
-        /// <exception cref="System.NullReferenceException">Thrown when a product with the given code does not exist.</exception>
-        /// <returns></returns>
+        /// <param name="productCode">A valid, unique code for the campaign. (e.g., P1, P2.)</param>
+        /// <param name="price">A decimal number that denotes the product's price.</param>
+        /// <param name="stock">A valid integer that shouldn't be negative at any time.</param>
+        /// <returns>
+        /// A message that depends on the success of the operation. Sample output:
+        /// <list type="bullet">
+        /// <item>Success: Product created; code P1, price 100, stock 1000</item>
+        /// <item>Failure: A product with the code P1 already exists.</item>
+        /// </list>
+        /// </returns>
         public string CreateProduct(string productCode, decimal price, int stock)
         {
             try
@@ -61,10 +66,16 @@ namespace Assignment
 
         }
         /// <summary>
-        /// 
+        /// Prepares a formatted string to indicate current status of a product.
         /// </summary>
-        /// <param name="productCode"></param>
-        /// <returns></returns>
+        /// <param name="productCode">A valid, unique product code for the product. (e.g., P1, P2.)</param>
+        /// <returns>
+        /// A message that depends on the success of the operation. Sample output:
+        /// <list type="bullet">
+        /// <item>Success: Product P1 info; price 100, stock 1000</item>
+        /// <item>Failure: Could not find a product with the code P2.</item>
+        /// </list>
+        /// </returns>
         public string GetProductInfo(string productCode)
         {
             try
@@ -88,7 +99,8 @@ namespace Assignment
         /// </list>
         /// </summary>
         /// <param name="productCode">A valid, unique code for the campaign. (e.g., P1, P2.)</param>
-        /// <param name="quantity"></param>
+        /// <param name="quantity">A number that shows how many of the given product is ordered.</param>
+        /// <returns>A message that depends on the success of the operation.</returns>
         public string CreateOrder(string productCode, int quantity)
         {
             // TODO: Check if such a product exists, warn if it doesnt.
@@ -140,29 +152,46 @@ namespace Assignment
         /// <param name="duration">A valid integer with the unit "hour(s)".</param>
         /// <param name="priceManipulationLimit">A valid integer that shows the percentage a price can be increased or decreased by.</param>
         /// <param name="targetSalesCount">The amount of the products that is targeted to be sold.</param>
+        /// <returns>
+        /// A message that depends on the success of the operation. Sample output:
+        /// <list type="bullet">
+        /// <item>Success: Campaign created; name C1, product P1, duration 10, limit 20, target sales count 100</item>
+        /// <item>Failure: Cannot create a campaign with the product code P1, since a campaign for this product already exists.</item>
+        /// <item>Failure: Cannot create a campaign for the product P1, since the product stock (10) is insufficient.</item>
+        /// </list>
+        /// </returns>
         public string CreateCampaign(string name, string productCode, int duration, int priceManipulationLimit, int targetSalesCount)
         {
             bool productExists = products.ContainsKey(productCode);
             if (productExists)
             {
-                try
+                int productStock = GetProduct(productCode).stock;
+                if (productStock >= targetSalesCount)
                 {
-                    Campaign newCampaign = new Campaign(name, productCode, duration, priceManipulationLimit, targetSalesCount);
-                    this.campaigns.Add(productCode, newCampaign);
-                    return
-                        $@"Campaign created; 
+                    try
+                    {
+                        Campaign newCampaign = new Campaign(name, productCode, duration, priceManipulationLimit, targetSalesCount);
+                        this.campaigns.Add(productCode, newCampaign);
+                        return
+                            $@"Campaign created; 
                         name {newCampaign.name}, 
                         product {newCampaign.productCode}, 
                         duration {newCampaign.duration}, 
                         limit {newCampaign.priceManipulationLimit}, 
                         target sales count {newCampaign.targetSalesCount}"
-                        .Replace(Environment.NewLine, "")
-                        .Replace("    ", "");
+                            .Replace(Environment.NewLine, "")
+                            .Replace("    ", "");
+                    }
+                    catch (System.ArgumentException)
+                    {
+                        return $"Cannot create a campaign with the product code {productCode}, since a campaign for this product already exists.";
+                    }
                 }
-                catch (System.ArgumentException)
+                else
                 {
-                    return $"Cannot create a campaign with the name {name}, since such a campaign already exists.";
+                    return $"Cannot create a campaign for the product {productCode}, since the product stock (productStock) is insufficient.";
                 }
+
             }
             else
             {
@@ -171,10 +200,16 @@ namespace Assignment
 
         }
         /// <summary>
-        /// 
+        /// Prepares a formatted string to indicate current status of a campaign.
         /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
+        /// <param name="name">A valid name for the campaign. (e.g., C1, C2.)</param>
+        /// <returns>
+        /// A message that depends on the success of the operation. Sample output:
+        /// <list type="bullet">
+        /// <item>Success: Campaign C1 info; Status Active, Target Sales 100, Total Sales 50, Turnover 5000, Average Item Price 100</item>
+        /// <item>Failure: Could not find a campaign with the name C2.</item>
+        /// </list>
+        /// </returns>
         public string GetCampaignInfo(string name)
         {
             try
@@ -182,7 +217,7 @@ namespace Assignment
                 Campaign theCampaign = this.campaigns.Values.FirstOrDefault(campaign => campaign.name == name);
                 string campaignStatus = theCampaign.isActive ? "Active" : "Ended";
                 string averageItemPrice = theCampaign.totalSales == 0 ? "-" : $"{theCampaign.turnover / theCampaign.totalSales}";
-                
+
                 return
                         $@"Campaign {theCampaign.name} info; 
                         Status {campaignStatus}, 
@@ -196,24 +231,43 @@ namespace Assignment
             catch (System.NullReferenceException)
             {
                 return $"Could not find a campaign with the name {name}.";
-                // We don't want to throw an exception, since we want program to keep going.
-                //throw;
+                // We don't actually throw the exception, since we want program to keep going.
+                // throw;
             }
         }
         /// <summary>
         /// This method advances time of the store and updates the following:
         /// <list type="bullet">
         /// <item>Price of the product, depending on demand coefficient.</item>
-        /// <item>Is active status of the campign, depending on duration and time.</item>
-        /// <item></item>
+        /// <item>Is active status of the campaign, depending on duration and time.</item>
+        /// <item>Demand coefficient which is used to determine price change for the campaign product.</item>
         /// </list>
         /// </summary>
-        /// <param name="time"></param>
+        /// <param name="time">A valid integer, since the time is incremented in hours.</param>
+        /// <returns>A message that depends on the success of the operation.</returns>
         public string IncreaseTime(int time)
         {
             TimeSpan timeToAdd = new TimeSpan(time, 0, 0);
             this.time = this.time.Add(timeToAdd);
             return $"Time is { this.time.ToString("HH:mm") }";
+        }
+        /// <summary>
+        /// GetProductInfo or CreateProduct does not return the object itself, but this method does.
+        /// </summary>
+        /// <param name="productCode">A valid, unique product code for the product. (e.g., P1, P2.)</param>
+        /// <returns>The product object if found, null otherwise.</returns>
+        public Product GetProduct(string productCode)
+        {
+            return this.products.GetValueOrDefault(productCode);
+        }
+        /// <summary>
+        /// GetCampaignInfo or CreateCampaign does not return the object itself, but this method does.
+        /// </summary>
+        /// <param name="productCode">A valid, unique product code for the product. (e.g., P1, P2.)</param>
+        /// <returns>The campaign object if found, null otherwise.</returns>
+        public Campaign GetCampaign(string productCode)
+        {
+            return this.campaigns.GetValueOrDefault(productCode);
         }
     }
 }
