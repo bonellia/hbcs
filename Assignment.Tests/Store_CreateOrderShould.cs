1da748fb27d264c27ba1f37f92203696d3bac3c1
Taha Yasin Erol <taha@bigtaus.com>
2020-10-11 13:00:46 +0300

    Add comments to highlight test arrangements.

--- Assignment.Tests/Store_CreateOrderShould.cs
+++ Assignment.Tests/Store_CreateOrderShould.cs
@@ -14,14 +14,17 @@ namespace Assignment.Tests
         [Fact]
         public void CreateOrder_ExistingProductHasStockNoCampaign_ReturnSuccessMessage()
         {
+            // Arrange the case scenario:
             var testStore = new Store();
-
             testStore.CreateProduct("P1", 100, 5);
             var newProduct = testStore.GetProduct("P1");
             var stockBeforeOrder = newProduct.stock;
+
+            // Act using the subject method:
             string createOrderResponse = testStore.CreateOrder("P1", 3);
             var stockAfterOrder = newProduct.stock;
 
+            // Assert the expected results.
             Assert.Equal("Order created; product P1, quantity 3", createOrderResponse);
             Assert.Equal(stockBeforeOrder-3, stockAfterOrder);
 
@@ -30,14 +33,17 @@ namespace Assignment.Tests
         [Fact]
         public void CreateOrder_ExistingProductNoStockNoCampaign_ReturnFailMessage()
         {
+            // Arrange the case scenario:
             var testStore = new Store();
-
             testStore.CreateProduct("P1", 100, 2);
             var newProduct = testStore.GetProduct("P1");
             var stockBeforeOrder = newProduct.stock;
-            string createOrderResponse = testStore.CreateOrder("P1", 3);
+
+            // Act using the subject method:
+            string createOrderResponse = testStore.CreateOrder("P1", 3);            
             var stockAfterOrder = newProduct.stock;
 
+            // Assert the expected results.
             Assert.Equal("Order failed; insufficient stock (2) for the product P1.", createOrderResponse);
             Assert.Equal(stockBeforeOrder, stockAfterOrder);
         }
@@ -45,18 +51,21 @@ namespace Assignment.Tests
         [Fact]
         public void CreateOrder_NonExistentProductNoCampaign_ReturnFailMessage()
         {
+            // Arrange the case scenario:
             var testStore = new Store();
 
+            // Act using the subject method:
             string createOrderResponse = testStore.CreateOrder("P1", 3);
 
+            // Assert the expected results.
             Assert.Equal("Order failed; no product with the product code P1.", createOrderResponse);
         }
 
         [Fact]
         public void CreateOrder_ExistingProductHasStockHasCampaign_ReturnSuccessMessage()
         {
+            // Arrange the case scenario:
             var testStore = new Store();
-
             testStore.CreateProduct("P1", 100, 200);
             var newProduct = testStore.GetProduct("P1");
             var stockBeforeOrder = newProduct.stock;
@@ -64,16 +73,18 @@ namespace Assignment.Tests
             var newCampaign = testStore.GetCampaign("P1");
             var totalSalesBeforeOrder = newCampaign.totalSales;
             var turnoverBeforeOrder = newCampaign.turnover;
-            string createOrderResponse = testStore.CreateOrder("P1", 3);
+
+            // Act using the subject method:
+            string createOrderResponse = testStore.CreateOrder("P1", 3);  
             var stockAfterOrder = newProduct.stock;
             var totalSalesAfterOrder = newCampaign.totalSales;
-            var turnoverAfterOrder = newCampaign.turnover;
+            var turnoverAfterOrder = newCampaign.turnover;          
 
+            // Assert the expected results.
             Assert.Equal("Order created; product P1, quantity 3", createOrderResponse);
             Assert.Equal(stockBeforeOrder-3, stockAfterOrder);
             Assert.Equal(totalSalesBeforeOrder+3, totalSalesAfterOrder);
             Assert.Equal(turnoverBeforeOrder+newProduct.price*3, turnoverAfterOrder);
-
         }
     }
 }
