41dc6d275be2d4faee414e93f10e6ed35c05b381
Taha Yasin Erol <taha@bigtaus.com>
2020-10-12 12:39:31 +0300

    Update IncreaseTime tests.

--- Assignment.Tests/Store_IncreaseTimeShould.cs
+++ Assignment.Tests/Store_IncreaseTimeShould.cs
@@ -1,24 +1,31 @@
 using Xunit;
+using System;
 using Assignment;
 
 namespace Assignment.Tests
 {
+    /// <summary>
+    /// <para>Only tests the text output of increaseTime.</para>
+    /// <para>The business rules are tested on other methods.</para>
+    /// </summary>
     public class Store_IncreaseTimeShould
     {
 
-        public Store_IncreaseTimeShould()
-        {
-
-        }
-
         [Fact]
-        public void IncreaseTime_ValidInput_ReturnSuccessMessage()
+        public void IncreaseTime_NoCampaign_ReturnSuccessMessage()
         {
-        //Given
+            // Arrange the case scenario:
+            var testStore = new Store();
+            var initialTime = testStore.time;
 
-        //When
-        
-        //Then
+
+            // Act using the subject method:
+            var increaseTimeResult = testStore.IncreaseTime(1);
+            DateTime updatedTime = initialTime.AddHours(1);
+
+            // Assert the expected results.
+            Assert.Equal("Time is 01:00", increaseTimeResult);
+            Assert.Equal(updatedTime, testStore.time);
         }
     }
 }
